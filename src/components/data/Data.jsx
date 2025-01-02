97dcde48c3535d595780b2c38b7ecd4f987da505
Nizar-Esmat <nizaresmat2000@gmail.com>
2025-01-02 20:40:58 +0200

    add spinner and fix the star bug

--- src/components/data/Data.jsx
+++ src/components/data/Data.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
-
+import { Card, Col, Row, Spinner } from 'react-bootstrap';
 
 export default function Data({ search }) {
     const [person, setPerson] = useState([]);
+    const [loading, setLoading] = useState(true);  
 
     useEffect(() => {
         fetch('users.json')
             .then((res) => res.json())
-            .then((data) => setPerson(data))
+            .then((data) => {
+                setPerson(data);
+                setLoading(false);  
+            })
             .catch((err) => {
                 console.log("There is an error:", err);
+                setLoading(false);  
             });
     }, []);
 
@@ -21,31 +25,36 @@ export default function Data({ search }) {
     return (
         <div>
             <h2>User List</h2>
-            <Row xs={1} md={2} lg={3} className="g-4">
-                {filteredPersons.map((data, index) => (
-                    <Col key={index}>
-                        <Card>
-                            <Card.Img variant="top" src={data.image} />
-                            <Card.Body>
-                                <Card.Title>{data.firstName}</Card.Title>
-                                <Card.Text>
-                                    <strong>Email:</strong> {data.email}
-                                    <br />
-                                    <strong>Phone:</strong> {data.phone}
-                                </Card.Text>
-                                <i
-                                    className="fa-solid fa-star"
-                                    style={{
-                                        color: data.eyeColor,
-                                        fontSize: '1.5rem',
-                                    }}
-                                ></i>
-
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+            {loading ? (  
+                <div className="d-flex justify-content-center">
+                   <Spinner animation="grow" />
+                </div>
+            ) : (
+                <Row xs={1} md={2} lg={3} className="g-4">
+                    {filteredPersons.map((data, index) => (
+                        <Col key={index}>
+                            <Card>
+                                <Card.Img variant="top" src={data.image} />
+                                <Card.Body>
+                                    <Card.Title>{data.firstName}</Card.Title>
+                                    <Card.Text>
+                                        <strong>Email:</strong> {data.email}
+                                        <br />
+                                        <strong>Phone:</strong> {data.phone}
+                                    </Card.Text>
+                                    <i
+                                        className="fa-solid fa-star"
+                                        style={{
+                                            color: data.eyeColor==="Green" ? "green" : "red",
+                                            fontSize: '1.5rem',
+                                        }}
+                                    ></i>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            )}
         </div>
     );
 }
